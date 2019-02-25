6dac73c1e633e7ac559625663b06551709a424bf
AdrianCabrera <adriancabsan98@gmail.com>
2019-02-25 10:53:52 -0600

    Upload first version

---
+++ lib/plug_ex.ex
@@ -0,0 +1,27 @@
+
+  defmodule PlugEx do
+    use Application
+    require Logger
+      @moduledoc """
+  Documentation for PlugEx.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PlugEx.hello()
+      :world
+
+  """
+    def start(_type, _args) do
+      port = Application.get_env(:plug_ex, :cowboy_port, 8000)
+      children = [
+        Plug.Adapters.Cowboy.child_spec(:http, PlugEx.Router, [], port: port)
+      ]
+      Logger.info "App Started"
+      Supervisor.start_link(children, strategy: :one_for_one)
+    end
+  end
+
